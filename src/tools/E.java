127f95de2e4d0ef097859429d4f4c2401cf2e43a
oliver <hopensic@gmail.com>
2015-05-28 23:40:55 +0800

    first

---
+++ src/tools/E.java
@@ -0,0 +1,89 @@
+package tools;
+
+public class E {
+
+	public static void println() {
+		System.err.println();
+	}
+
+	public static void rint(String s) {
+		System.err.print(s);
+	}
+
+	public static void lnrint(String s) {
+		System.err.println(s);
+	}
+
+	public static void rint(Object s) {
+		System.err.print(s);
+	}
+
+	public static void lnrint(Object s) {
+		System.err.println(s);
+	}
+
+	public static void rint(int s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(int s) {
+		System.err.println(s);
+	}
+
+	public static void rint(short s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(short s) {
+		System.err.println(s);
+	}
+
+	public static void rint(char s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(char s) {
+		System.err.println(s);
+	}
+
+	public static void rint(double s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(double s) {
+		System.err.println(s);
+	}
+
+	public static void rint(float s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(float s) {
+		System.err.println(s);
+	}
+
+	public static void rint(byte s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(byte s) {
+		System.err.println(s);
+	}
+
+	public static void rint(boolean s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(boolean s) {
+		System.err.println(s);
+	}
+
+	public static void rint(long s) {
+		System.err.println(s);
+	}
+
+	public static void lnrint(long s) {
+		System.err.println(s);
+	}
+
+}
---
+++ src/tools/P.java
@@ -0,0 +1,85 @@
+package tools;
+
+public class P {
+
+	public static void rint(String s) {
+		System.out.print(s);
+	}
+
+	public static void lnrint(String s) {
+		System.out.println(s);
+	}
+
+	public static void rint(Object s) {
+		System.out.print(s);
+	}
+
+	public static void lnrint(Object s) {
+		System.out.println(s);
+	}
+
+	public static void rint(int s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(int s) {
+		System.out.println(s);
+	}
+
+	public static void rint(short s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(short s) {
+		System.out.println(s);
+	}
+
+	public static void rint(char s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(char s) {
+		System.out.println(s);
+	}
+
+	public static void rint(double s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(double s) {
+		System.out.println(s);
+	}
+
+	public static void rint(float s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(float s) {
+		System.out.println(s);
+	}
+
+	public static void rint(byte s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(byte s) {
+		System.out.println(s);
+	}
+
+	public static void rint(boolean s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(boolean s) {
+		System.out.println(s);
+	}
+
+	public static void rint(long s) {
+		System.out.println(s);
+	}
+
+	public static void lnrint(long s) {
+		System.out.println(s);
+	}
+
+}
